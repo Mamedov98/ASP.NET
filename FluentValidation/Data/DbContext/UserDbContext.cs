d32feefabd41bfe44498a86f4c636430c3d17ce7
Mamedov98 <110538493+Mamedov98@users.noreply.github.com>
2023-08-25 02:07:59 +0400

    DbFluentApi

---
+++ FluentValidation/Data/DbContext/UserDbContext.cs
@@ -0,0 +1,50 @@
+﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+
+
+public class UserDbContext : DbContext
+{
+    public DbSet<User> Users { get; set; }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        // parol123
+        string connectionString = "Data Source=sql.bsite.net\\MSSQL2016;Initial Catalog=mamedov98_DataFluentValidation;User ID=mamedov98_DataFluentValidation;Password=parol123;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+        optionsBuilder.UseSqlServer(connectionString);
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.ToTable("Users");
+
+            entity.HasKey(e => e.Id);
+
+            entity.Property(e => e.Name)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            entity.Property(e => e.Surname)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            entity.Property(e => e.Username)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            entity.Property(e => e.Password)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            entity.Property(e => e.Email)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            entity.Property(e => e.Age)
+                .IsRequired();
+        });
+    }
+
+
+}
