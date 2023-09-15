213bedd5a34f4f7672c1dcaf33a68992c9195c3f
Mamedov98 <110538493+Mamedov98@users.noreply.github.com>
2023-09-15 20:56:31 +0400

    Content-Type error

---
+++ MinimapApiCar/MinimapApiCar/DbContextt/DbContextCar.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MinimapApiCar.DbContextt
+{
+    public class DbContextCar : DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+        public DbContextCar(DbContextOptions<DbContextCar> options) : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=CarDataBaseApi;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Car>(entity =>
+            {
+                entity.HasKey(x => x.Id);
+
+                entity.Property(x => x.Name)
+                .IsRequired();
+                entity.Property(entity => entity.Model)
+                .IsRequired();
+                entity.Property(entity => entity.Price)
+                .IsRequired();
+                entity.Property(entity => entity.Year);
+            });
+        }
+    }
+}
---
+++ MinimapApiCar/MinimapApiCar/Models/Car.cs
@@ -0,0 +1,10 @@
+﻿public class Car
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Model { get; set; }
+    public int Year { get; set; }
+    public int Price { get; set; }
+
+}
+
